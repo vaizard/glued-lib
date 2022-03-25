dd1630599453093caf04df779ca55835a844638e
Pavel Stratil <pavel@vaizard.org>
2022-03-25 22:47:10 +0000

    code cleanup and documentation wip

--- src/Utils.php
+++ src/Utils.php
@@ -6,11 +6,9 @@ use Slim\Routing\RouteContext;
 class Utils
 {
 
-
     protected $db;
     protected $settings;
     protected $routecollector;
-
 
     public function __construct($db, $settings, $routecollector) {
         $this->db = $db;
@@ -18,7 +16,83 @@ class Utils
         $this->routecollector = $routecollector;
     }
 
-    public function default_locale(string $language): string {
+
+    ////////////////////////////////////////////////////////////////////
+    // ARRAYS MANIPULTAION                                            //
+    ////////////////////////////////////////////////////////////////////
+
+    public function array_unflatten($collection) {
+        $collection = (array) $collection;
+        $output = array();
+        foreach ($collection as $key => $value) {
+            $this->array_set( $output, $key, $value );
+            if (is_array($value) && !strpos($key, '.')) {
+                $nested = array_unflatten( $value );
+                $output[$key] = $nested;
+            }
+        }
+        return $output;
+    }
+
+    public function array_set(&$array, $key, $value)  {
+        if (is_null($key)) { return $array = $value; }
+        $keys = explode( '.', $key );
+        while (count($keys) > 1) {
+            $key = array_shift( $keys );
+            // If the key doesn't exist at this depth, we will just create an empty array
+            // to hold the next value, allowing us to create the arrays to hold final
+            // values at the correct depth. Then we'll keep digging into the array.
+            if (!isset($array[$key]) || !is_array($array[$key])) {
+                $array[$key] = array();
+            }
+            $array =& $array[$key];
+        }
+        $array[array_shift($keys)] = $value;
+        return $array;
+    }
+
+
+    ////////////////////////////////////////////////////////////////////
+    // STRINGS MANIPULTAION                                           //
+    ////////////////////////////////////////////////////////////////////
+
+    /**
+     * Replaces the nth occurrence of a substring in a string
+     * 
+     * @param string $search      Search string
+     * @param string $replace     Replace string
+     * @param string $subject     Source string
+     * @param int $occurrence     N-th occurrence
+     * @return type               Replaced string
+     */
+    public function str_replace_n($search, $replace, $subject, $occurrence): string {
+        $search = preg_quote($search);
+        return preg_replace("/^((?:(?:.*?$search){".--$occurrence."}.*?))$search/", "$1$replace", $subject);
+    }
+
+    /**
+     * Concatenates items into a string using a delimeter.
+     * Excessive delimeters get trimmed.
+     * 
+     * @param string $delimeter     The delimeter
+     * @param array $arrayOfStrings Tha array to be concatenated
+     * @return string               Concatenated string
+     */
+    public function concat($delimeter, array $arrayOfStrings): string {
+        return trim(implode($delimeter, array_filter(array_map('trim',$arrayOfStrings))));
+    }
+
+
+    ////////////////////////////////////////////////////////////////////
+    // LOCALIZATION                                                   //
+    ////////////////////////////////////////////////////////////////////
+
+    /**
+     * Converts the two-letter language code into the default locale.
+     * @param string $language ISO 639-1 Code (i.e. `en`)
+     * @return string The most common locale associated with $language (i.e. `en_US`)
+     */
+    public function get_default_locale(string $language): string {
         $def_locales = [
             "af" => "af_ZA",
             "ar" => "ar",
@@ -67,88 +141,32 @@ class Utils
             "vi" => "vi_VN",
             "zh" => "zh_CN"
         ];
-        if(isset($def_locales[$language])) { return $def_locales[$language]; }
+        if (isset($def_locales[$language])) { return $def_locales[$language]; }
         else { return 'en_US'; }
 
     }
 
 
-    public function sql_insert_with_json($table, $row) {
-        $this->db->startTransaction(); 
-        $id = $this->db->insert($table, $row);
-        $err = $this->db->getLastErrno();
-        if ($id) {
-          $updt = $this->db->rawQuery("UPDATE `".$table."` SET `c_json` = JSON_SET(c_json, '$.id', ?) WHERE c_uid = ?", Array ((int)$id, (int)$id));
-          $err += $this->db->getLastErrno();
-        }
-        if ($err === 0) { $this->db->commit(); } else { $this->db->rollback(); throw new \Exception(__('Database error: ')." ".$err." ".$this->db->getLastError()); }
-        return (int)$id;
-    }
+    ////////////////////////////////////////////////////////////////////
+    // SLIM ROUTING HELPERS                                           //
+    ////////////////////////////////////////////////////////////////////
 
-
-    public function fetch_uri($uri, $extra_opts = []) {
-        $curl_handle = curl_init();
-        $extra_opts[CURLOPT_URL] = $uri; 
-        $curl_options = array_replace( $this->settings['curl'], $extra_opts );
-        curl_setopt_array($curl_handle, $curl_options);
-        $data = curl_exec($curl_handle);
-        curl_close($curl_handle);
-        return $data;
-    }
-
-
-    // will concatenate array items with delimeter, then trim the result
-    public function concat($delimeter, array $arrayOfStrings): string {
-      return trim(implode($delimeter, array_filter(array_map('trim',$arrayOfStrings))));
-    }
-
-
-    public function array_unflatten($collection) {
-        $collection = (array) $collection;
-        $output = array();
-        foreach ($collection as $key => $value) {
-            $this->array_set( $output, $key, $value );
-            if (is_array($value) && !strpos($key, '.')) {
-                $nested = array_unflatten( $value );
-                $output[$key] = $nested;
-            }
-        }
-        return $output;
-    }
-
-
-    public function array_set(&$array, $key, $value)  {
-        if (is_null($key)) { return $array = $value; }
-        $keys = explode( '.', $key );
-        while (count($keys) > 1) {
-            $key = array_shift( $keys );
-            // If the key doesn't exist at this depth, we will just create an empty array
-            // to hold the next value, allowing us to create the arrays to hold final
-            // values at the correct depth. Then we'll keep digging into the array.
-            if (!isset($array[$key]) || !is_array($array[$key])) {
-                $array[$key] = array();
-            }
-            $array =& $array[$key];
-        }
-        $array[array_shift($keys)] = $value;
-        return $array;
+    public function get_current_route($request): string {
+        $routeContext = RouteContext::fromRequest($request);
+        $route = $routeContext->getRoute();
+        return $route->getName();
     }
 
     /**
-     * String replace nth occurrence
-     * 
-     * @param type $search      Search string
-     * @param type $replace     Replace string
-     * @param type $subject     Source string
-     * @param type $occurrence  Nth occurrence
-     * @return type         Replaced string
+     * get_routes_array() will
+     * - get slim routes from the route collector and
+     * - append the pattern and implemented methods
+     * - append the route name
+     * - append the label and icon (from configuration)
+     * and return all this as an indexed array.
+     * @return array [description]
      */
-    public function str_replace_n($search, $replace, $subject, $occurrence): string {
-        $search = preg_quote($search);
-        return preg_replace("/^((?:(?:.*?$search){".--$occurrence."}.*?))$search/", "$1$replace", $subject);
-    }
-
-    public function get_routes($named = false): array {
+    public function get_routes_array(): array {
         $routes = $this->routecollector->getRoutes();
         foreach ($routes as $route) {
             $i = $route->getPattern();
@@ -167,25 +185,28 @@ class Utils
         return array_values($data);
     }
 
-    public function get_named_routes($named = false): array {
-        $routes = $this->get_routes();
+    /**
+     * get_routes_map() turns the indexed array into an associative array (map).
+     * The key is the route name. get_routes_map() is used by get_routes_tree().
+     * @return array [description]
+     */
+    private function get_routes_map(): array {
+        $routes = $this->get_routes_array();
         foreach ($routes as $route) {
             if ($route['name']) { $res[$route['name']] = $route; }
         }
         return $res;
     }
 
-    public function get_current_route($request): string {
-        $routeContext = RouteContext::fromRequest($request);
-        $route = $routeContext->getRoute();
-        return $route->getName();
-    }
-
-    public function get_navigation($current_route = null): array {
-
-        $routes = $this->get_named_routes(true);
+    /**
+     * get_routes_tree() returns routes as the leafs of a 3 dimensional array.
+     * 2nd dimension nodes group routes of one microservice.
+     * 1st dimension nodes group routes according to usage (interface, api, backend)
+     * @return array [description]
+     */
+    public function get_routes_tree($current_route = null): array {
+        $routes = $this->get_routes_map();
         foreach ($routes as $k => $v) {
-
             // 3rd tier nodes (route within a microservice)
             $path = explode( '.', $k);
             $item = $v;
@@ -197,14 +218,12 @@ class Utils
             $item['name'] = $k;
             $item['type'] = 'route';
             $r[$path[0]]['children'][$path[1]]['children'][]['node'] = $item;
-
             // 2nd tier nodes (routegroup of a microservice: i.e. core, skeleton ...)
             $item = null;
             $item['label'] = $this->settings['routes'][$path[0].'.'.$path[1]]['label'] ?? null;
             $item['icon'] = $this->settings['routes'][$path[0].'.'.$path[1]]['icon'] ?? null;
             $item['type'] = 'routegroup';
             $r[$path[0]]['children'][$path[1]]['node'] = $item;
-
             // 1st tier nodes (routegroup class: app, api)
             $item['label'] = $this->settings['routes'][$path[0]]['label'] ?? null;
             $item['icon'] = $this->settings['routes'][$path[0]]['icon'] ?? null;
@@ -214,6 +233,41 @@ class Utils
         $r = array_merge_recursive($r, $append);
         return $r;
     }
+
+
+    ////////////////////////////////////////////////////////////////////
+    // SQL HELPERS                                                    //
+    ////////////////////////////////////////////////////////////////////
+
+    public function sql_insert_with_json($table, $row) {
+        $this->db->startTransaction(); 
+        $id = $this->db->insert($table, $row);
+        $err = $this->db->getLastErrno();
+        if ($id) {
+          $updt = $this->db->rawQuery("UPDATE `".$table."` SET `c_json` = JSON_SET(c_json, '$.id', ?) WHERE c_uid = ?", Array ((int)$id, (int)$id));
+          $err += $this->db->getLastErrno();
+        }
+        if ($err === 0) { $this->db->commit(); } else { $this->db->rollback(); throw new \Exception(__('Database error: ')." ".$err." ".$this->db->getLastError()); }
+        return (int)$id;
+    }
+
+
+    ////////////////////////////////////////////////////////////////////
+    // CURL HELPERS                                                   //
+    ////////////////////////////////////////////////////////////////////
+
+    public function fetch_uri($uri, $extra_opts = []) {
+        $curl_handle = curl_init();
+        $extra_opts[CURLOPT_URL] = $uri; 
+        $curl_options = array_replace( $this->settings['curl'], $extra_opts );
+        curl_setopt_array($curl_handle, $curl_options);
+        $data = curl_exec($curl_handle);
+        curl_close($curl_handle);
+        return $data;
+    }
+
+
+
 
 
 }
