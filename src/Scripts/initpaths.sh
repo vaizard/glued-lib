c1e81491cae285b10dd3570e5517b5f3282d0423
Pavel Stratil <pavel@vaizard.org>
2022-06-18 11:00:29 +0000

    add share folder

--- src/Scripts/initpaths.sh
+++ src/Scripts/initpaths.sh
@@ -6,7 +6,10 @@ source "$DIR/loadenv.sh"
 mkdir -p "${datapath}/$(basename `pwd`)/backups";
 mkdir -p "${datapath}/$(basename `pwd`)/config";
 mkdir -p "${datapath}/$(basename `pwd`)/cache";
+mkdir -p "${datapath}/$(basename `pwd`)/share";
 mkdir -p "${datapath}/$(basename `pwd`)/data";
+
 chown www-data:www-data "${datapath}/$(basename `pwd`)/data"
 chown www-data:www-data "${datapath}/$(basename `pwd`)/cache"
+chmod 777 "${datapath}/$(basename `pwd`)/share"
 echo "[PASS] datapaths exist"
