44778a3b59b152352b0c87b8c5a399a478b37fdb
Pavel Stratil <pavel@vaizard.org>
2022-08-12 21:31:04 +0000

    exit on cache rebuild failure

--- src/Scripts/cacheroutes.sh
+++ src/Scripts/cacheroutes.sh
@@ -4,7 +4,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/loadenv.sh"
 source "$DIR/loadenv.sh"
 
+echo "[INFO] rebuilding service routes cache"
+
 if [ -f "glued/Config/routes.yaml" ]; then
   cp -r glued/Config/routes.yaml "${datapath}/$(basename `pwd`)/cache/routes.yaml";
+  echo "[PASS] routes rebuilt."
+  echo ""
+else
+  echo "[FAIL] routes failed to rebuild."
+  echo ""
+  exit 1
 fi
-echo "[PASS] service routes cached"
