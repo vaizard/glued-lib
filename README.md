f2813b9a697755a2b54aaf41303e8890a8995731
Pavel Stratil <pavel@vaizard.org>
2023-04-09 22:36:19 +0000

    add notify

--- README.md
+++ README.md
@@ -1,2 +1,145 @@
 # glued-lib
 Toolkit and dependency abstraction library for the glued project.
+
+## Notifications
+
+The notify class uses for most cases the symfony/notifier libraries under the hood, but provides
+enough abstraction to enable easy integration of other code to communicate on platforms that are
+unsupported by symfony.
+
+The class requires a yaml configuration, where `dst` in every netowrk is an administrators'
+destination (recipient) address. In each network (i.e. `email`), multiple (sending) channels can be
+used. Currently, if the first channel fails to send, the class will try to continue sending by other
+channels. This failover mechanism (intentionally) doesn't work between networks over privacy/security
+concerns.
+
+In case of admins, all destinations on all networks are used to really ensure admins get notified.
+In case of users, users may change define one or more notification channels to be used always and
+additional channels that can be used only by user choice.
+
+### Configuration
+
+To configure notifications, override defaults.yaml. An example here:
+
+```yaml
+notify:
+    network:
+        telegram:
+            channels:
+                - name: Telegram
+                  dsn: 'telegram://123456789:yourbotsecret@default?channel='
+            dst:
+                - '111111111'
+                - '222222222'
+            config:
+                src: @MyTelegramBot
+        email:
+            channels:
+                - name: E-mail (smtp)
+                  dsn: 'smtp://login:pass@mx1.example.com:587?encryption=starttls'
+                - name: E-mail (smtp2)
+                  dsn: 'smtp://login:pass@mx2.example.com:587?encryption=starttls'
+            config:
+                src: 'sender@example.com'
+            dst:
+                - 'recipient@gmail.com'
+                - 'someotheradmin@outlook.com'
+        sms:
+            channels:
+                - name: Twilio
+                  dsn: 'twilio://SID:TOKEN@default?from=FROM'
+                - name: O2
+            config:
+                src: '+1123456789'
+```
+
+### Usage
+
+Send a notification to admins
+
+```php
+$this->notify->send(content: 'this is the message', subject: 'this is the optional header (i.e. mail subject', notify_admins: true);
+```
+
+Send a notification to specific users (all the channels below will be used)
+
+```php
+$users = [
+    '45216b0c-32ca-4a48-8307-3466ee81f32e' => [
+        'telegram'  => '222333222',
+        'email' => 'user@example.com',
+    ],
+    '8e51819a-4595-4e5c-990c-f27cacb1a2dd' => [
+        'telegram' => '2222777777',
+    ],
+];
+$this->notify->to($users)->send('A quick notification only sent to $users');
+```
+
+Helper functions of the class will
+
+```php
+$res = $this->notify->getusers();
+echo "<br><br>All users to be notified:";
+print_r($res); 
+
+$res = $this->notify->getadmins();
+echo "<br><br>All admins to be notified:";
+print_r($res); 
+
+$res = $this->notify->status();
+print_r($res);
+echo "<br><br>Current configuration and send queue:";
+```
+
+### Supported network/channels are supported
+
+Currently only the following networks/channels are supported:
+
+- TELEGRAM
+- EMAIL (SMTP)
+
+**TELEGRAM**
+
+Go to `https://web.telegram.org/k/#@BotFather` and type in
+
+```
+/newbot         # start setup
+glued-dev       # bot name
+glued_dev_bot   # bot username
+```
+
+Go to `https://web.telegram.org/k/#@RawDataBot` to get the chat_id of the recipient
+
+You will end up with something like:
+
+- Token: `1234567890:asecretstringapproximatelythisloong`
+- Uri: `https://web.telegram.org/k/#@glued_dev_bot`
+- Chat_id: `2244668800`
+
+and a DSN string `TELEGRAM_DSN=telegram://1234567890:asecretstringapproximatelythisloong@default?channel=2244668800`
+
+To test that the above works, use curl as follows:
+
+```bash
+TG_TOKEN=secret-token
+TG_CHAT_ID=recipients-chat-id
+curl -X POST -H "Content-Type:multipart/form-data" -F chat_id=$TG_CHAT_ID -F text="message" "https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+```
+
+**EMAIL**
+
+To test, use curl as follows:
+
+```bash
+SMTP_HOST='mail.example.com:587'
+SMTP_USER='login:pass'
+SMTP_FROM='sender@example.com'
+SMTP_RCPT='recipient@example.com'
+
+curl --ssl-reqd --url "smtp://$SMTP_HOST" --user "$SMTP_USER" --mail-from "$SMTP_FROM" --mail-rcpt "$SMTP_RCPT" --upload-file file.txt
+cat file.txt
+From: <test@noreply.com>
+To: <pavel@vaizard.org>
+Subject: Curl Test
+```
---
+++ src/Notify.php
@@ -0,0 +1,166 @@
+<?php
+declare(strict_types=1);
+namespace Glued\Lib;
+use Http\Client\Exception;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport\Dsn as MailerDsn;
+use Symfony\Component\Mailer\Transport\FailoverTransport;
+use Symfony\Component\Mailer\Transport\Smtp\SmtpTransport;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransportFactory;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Notifier\Message\EmailMessage;
+use Symfony\Component\Notifier\Message\ChatMessage;
+use Symfony\Component\Notifier\Message\SmsMessage;
+use Symfony\Component\Notifier\Message\PushMessage;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\Notifier;
+use Symfony\Component\Notifier\Recipient\EmailRecipientInterface;
+use Symfony\Component\Notifier\Bridge\Symfony\Mailer\NotifierSymfonyMailerTransport;
+use Symfony\Component\Notifier\Bridge\Symfony\Mailer\SymfonyMailerTransport;
+use Symfony\Component\Notifier\Transport\Dsn as NotifierDsn;
+use Symfony\Component\Notifier\Bridge\Telegram\TelegramTransportFactory;
+use Symfony\Component\Notifier\Chatter;
+use Symfony\Component\Notifier\Channel\ChatChannel;
+use Symfony\Component\Notifier\Channel\EmailChannel;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\RawMessage;
+use Symfony\Component\Mailer\Envelope;
+
+class Notify
+{
+
+    protected $recipients;
+    protected $settings;
+    protected $logger;
+    protected $adminDsts;
+
+    public function __construct($settings, $logger)
+    {
+        $this->settings = $settings['notify'];
+        $this->logger = $logger;
+        $this->users = [];
+        // on class invocation, get the admin destinations from $this->settings
+        $this->admins = $this->destinations();
+        // remove admin destinations (the method actually clears all the destinations, but at class invocation
+        // only admin destinations are defined). To later send notifications to admins, $this->settings needs to be
+        // merged with $this->admins
+        $this->clear_to();
+    }
+
+
+    public function destinations(): array {
+        foreach ($this->settings['network'] as $k => $v) {
+            $ret[$k] = $v['dst'] ?? [];
+        }
+        return $ret ?? [];
+    }
+
+    public function getadmins(): array {
+        return $this->admins;
+    }
+
+    public function clear_to(): self {
+        foreach ($this->settings['network'] as $k => &$v) {
+            $v['dst'] = [];
+        }
+        return $this;
+    }
+
+    public function getusers(): array {
+        return $this->users;
+    }
+
+    public function to(array $r): self {
+        $this->users = array_merge_recursive($this->users, $r);
+        foreach ($this->settings['network'] as $channel => &$config) {
+            $recipients = $config['dst'] ?? [];
+            $newRecipients = array_filter(array_column($r, $channel));
+            $recipients = array_unique(array_merge($recipients, $newRecipients));
+            $config['dst'] = $recipients;
+        }
+        return $this;
+    }
+
+    public function status(): array {
+        return $this->settings;
+    }
+
+    public function send(string $content, string $subject = 'Glued notification', bool $notify_admins = false)
+    {
+        $c = $this->settings['network'];
+        if ($notify_admins === true) {
+            $this->settings['network'] = array_merge_recursive($this->settings['network'], $this->getadmins());
+        }
+        $chat = new ChatMessage($content);
+        $push = new PushMessage($subject, $content);
+        $mail = (new Email())
+            ->from($c['email']['config']['src'])
+            ->subject($subject)
+            ->text($content);
+
+        foreach ($c as $type => $network) {
+            // ===========================================================
+            // Channels requiring a separate transport for every recipient
+            // ===========================================================
+            if ($type == 'telegram') {
+                $success = false; // Flag to help iterate over all provided channel DSNs
+                foreach ($c[$type]['channels'] as $channel) {
+                    try {
+                        foreach ($network['dst'] as $key => $dst) {
+                            $dsn = new NotifierDsn($channel['dsn'] . $dst);
+                            $transport = (new TelegramTransportFactory)->create($dsn);
+                            $transport->send($chat);
+                            unset($c[$type]['dst'][$key]); // On successful send remove destination (recipient)
+                        }
+                        $success = true;
+                        break; // Exit the loop if the email was sent successfully with current DSN
+                    } catch (\Exception $e) {
+                        // TODO Handle the exception better (e.g. log it)
+                        continue; // Try the next DSN in the list
+                    }
+                }
+                if (!$success) {
+                    $this->logger->error("lib.notify failed to send some " . $type . " notifications.");
+                }
+            }
+
+            // ===========================================================
+            // Channels ok with a single transport for multiple recipients
+            // ===========================================================
+            elseif ($type == 'email') {
+                $success = false; // Flag to help iterate over all provided channel DSNs
+                foreach ($c[$type]['channels'] as $channel) {
+                    $dsn = MailerDsn::fromString($channel['dsn']);
+                    try {
+                        $transport = (new EsmtpTransportFactory)->create($dsn);
+                        foreach ($network['dst'] as $key => $dst) {
+                            $envelope = new Envelope(new Address($c[$type]['config']['src']), [new Address($dst)]);
+                            $transport->send(new RawMessage($mail->to($dst)->toString()), $envelope);
+                            unset($c[$type]['dst'][$key]); // On successful send remove destination (recipient)
+                        }
+                        $success = true;
+                        break; // Exit the loop if the email was sent successfully with current DSN
+                    } catch (\Exception $e) {
+                        // TODO Handle the exception better (e.g. log it)
+                        continue; // Try the next DSN in the list
+                    }
+                }
+                if (!$success) {
+                    $this->logger->error("lib.notify failed to send some " . $type . " notifications.");
+                }
+            }
+
+            // ===========================================================
+            // Unhandled channels
+            // ===========================================================
+            else {
+                $this->logger->error("lib.notify configured to send unsupported type of notifications (" . $type . ").");
+            }
+        }
+    }
+
+    // TODO: add a property to store recipients to which the notification could not be sent successfully
+    //       basically just extract the recipients from $this->config after send()
+
+}
