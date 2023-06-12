fb37cdd7eb3a5c911ad6e32fdbaae2fcaf9026cd
Pavel Stratil <pavel@vaizard.org>
2023-06-12 20:42:47 +0000

    DEBUG

--- src/QuerySelect.php
+++ src/QuerySelect.php
@@ -58,7 +58,7 @@ class QuerySelect implements QueryInterface
 
     public function __toString(): string
     {
-        return 'SELECT FROM ' . $this->query
+        return $this->query
             . ($this->leftJoin === [] ? '' : ' LEFT JOIN '. implode(' LEFT JOIN ', $this->leftJoin))
             . ($this->innerJoin === [] ? '' : ' INNER JOIN '. implode(' INNER JOIN ', $this->innerJoin))
             . ($this->conditions === [] ? '' : ' WHERE ' . implode(' AND ', $this->conditions))
