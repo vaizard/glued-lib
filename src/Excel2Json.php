8ba0302cc1879e89ca07a28997d9bb36084a24d8
Pavel Stratil <pavel@vaizard.org>
2023-08-28 12:53:57 +0000

    add nonexisting sheet exception

--- src/Excel2Json.php
+++ src/Excel2Json.php
@@ -37,6 +37,8 @@ class Excel2Json
         $reader = IOFactory::createReader('Xlsx');
         $reader->setReadDataOnly(true);
         $spreadsheet = $reader->load($filePath);
+        $sheet = $spreadsheet->getSheetByName($sheetName);
+        if (is_null($sheet)) { throw new \Exception('Sheet '.$sheet. ' not in file '.$filePath)}
         $sheetData = $spreadsheet->getSheetByName($sheetName)->toArray(null, true, true, true);
 
         // fill $jsonPath AS $jsonPath[$column][$path]
