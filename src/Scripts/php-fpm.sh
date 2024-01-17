2839856cbdbb13c1416fd413194b160e5a7bc1ae
Pavel Stratil <pavel@vaizard.org>
2024-01-17 23:45:13 +0000

    handle dir not exists

--- src/Scripts/php-fpm.sh
+++ src/Scripts/php-fpm.sh
@@ -5,5 +5,6 @@ source "$DIR/loadenv.sh"
 source "$DIR/loadenv.sh"
 
 [[ $(lsb_release -is) != 'Ubuntu' ]] && echo "[WARN] Unsupported OS/Distro. Please ensure that PHP-FPM is reconfigured correctly."
-cp -r ./glued/Config/Php/* /etc/php/$(php -v | awk '/^PHP/ {print $2}' | cut -f1,2 -d.)/fpm/conf.d
-
+SRC_DIR="./glued/Config/Php"
+DST_DIR="/etc/php/$(php -v | awk '/^PHP/ {print $2}' | cut -f1,2 -d.)/fpm/conf.d"
+[ -d "$SRC_DIR" ] && cp -r "$SRC_DIR"/* "$DST_DIR"
