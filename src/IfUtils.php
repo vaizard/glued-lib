c45fd8649dfc694511e1c1f00e2ec609acfdf44f
Pavel Stratil <pavel@vaizard.org>
2024-01-27 19:38:21 +0000

    partial update of old ifutils queries

--- src/IfUtils.php
+++ src/IfUtils.php
@@ -6,21 +6,21 @@ namespace Glued\Lib;
  */
 class IfUtils
 {
-    protected $db;
-    public function __construct($db) {
-        $this->db = $db;
+    protected $mysqli;
+    public function __construct($mysqli) {
+        $this->mysqli= $mysqli;
     }
 
     public function logRequest($act_uuid): string
     {
         $run_uuid = \Ramsey\Uuid\Uuid::uuid4()->toString();
         $q = "
-            INSERT INTO `t_if__logs` 
+            INSERT INTO `t_if__runs` 
                 (`c_act_uuid`, `c_uuid`, `c_data`, `c_ts_requested`, `c_ts_responded`, `c_status`, `c_response_hash`, `c_response_fid`)
                 VALUES 
                 (uuid_to_bin(?, 1), uuid_to_bin(?, 1), '{}', now(), NULL, 'started', NULL, NULL);
             ";
-        $this->db->rawQuery($q, [$act_uuid, $run_uuid]);
+        $this->mysqli->execute_query($q, [$act_uuid, $run_uuid]);
         return $run_uuid;
     }
 
@@ -37,13 +37,14 @@ class IfUtils
                 c_status,
                 c_response_hash,
                 c_response_fid
-            FROM `t_if__logs`
+            FROM `t_if__runs`
             ";
         $qs = (new \Glued\Lib\QueryBuilder())->select($qs);
         if (!is_null($response_hash)) { $qs->where("c_response_hash = ?"); $qp[] = $response_hash; }
         if (!is_null($response_fid)) { $qs->where("c_response_fid = ?"); $qp[] = $response_fid; }
-        $r = $this->db->rawQuery((string) $qs, $qp);
-        return $r;
+        $res = $this->mysqli->execute_query((string) $qs, $qp);
+        foreach ($res as $i) { return $i ?? []; }
+        return [];
     }
 
 
@@ -51,7 +52,7 @@ class IfUtils
     {
         if (array_key_exists($status, ['failed', 'ok', 'started', 'skipped'])) throw new \Exception('Bad run status.');
         $q = "
-            UPDATE `t_if__logs` SET
+            UPDATE `t_if__runs` SET
                 `c_data` = ?,
                 `c_ts_responded` = now(),
                 `c_status` = ?,
@@ -59,10 +60,11 @@ class IfUtils
                 `c_response_fid` = ?
             WHERE `c_uuid` = uuid_to_bin(?, 1);
             ";
-        $this->db->rawQuery($q, [$data, $status, $response_hash, $response_fid, $run_uuid]);
+        $this->mysqli->execute_query($q, [$data, $status, $response_hash, $response_fid, $run_uuid]);
+
     }
 
-    
+
     public function getAction(mixed $action): array
     {
         $action = $args['uuid'] ?? false;
