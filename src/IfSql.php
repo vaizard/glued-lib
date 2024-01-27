e6a444f30d65b881f66c7e842e9c7395e67bd2c1
Pavel Stratil <pavel@vaizard.org>
2024-01-27 16:09:22 +0000

    updt

--- src/IfSql.php
+++ src/IfSql.php
@@ -9,108 +9,89 @@ class IfSql
     public $q;
     public function __construct()
     {
-        $q['logs'] = "
-        SELECT * FROM (
+
+        $q['rows:runs:simple'] = "
             SELECT 
                BIN_TO_UUID(`c_uuid`,1) AS `uuid`,
-               BIN_TO_UUID(`c_act_uuid`,1) AS `act_uuid`,
-               c_ts_requested as `ts_requested`,
-               c_ts_responded as `ts_responded`,
+               BIN_TO_UUID(`c_action_uuid`,1) AS `act_uuid`,
+               c_ts_start as `ts_started`,
+               c_ts_finish as `ts_finished`,
                c_data as `data`,
-               TIMESTAMPDIFF(SECOND, c_ts_requested, c_ts_responded) AS duration,
                c_status AS status,
-               c_response_hash as response_hash,
-               c_response_fid as response_fid,
-               TIMESTAMPDIFF(SECOND, c_ts_requested, NOW()) AS requested_ago
-            FROM `t_if__logs`
-        ) q
+               c_hash as response_hash,
+               c_fid as response_fid,
+               TIMESTAMPDIFF(SECOND, c_ts_start, c_ts_finish) AS run_time,
+               TIMESTAMPDIFF(SECOND, c_ts_finish, NOW()) AS run_ago
+            FROM `t_if__runs`
         ";
 
-        $q['queue-private'] = "
-        SELECT
-          svc_uuid,
-          svc_data,
-          svc_type,
-          svc_name,
-          act_uuid,
-          act_type,
-          act_freq,
-          log_req,
-          log_res,
-          log_duration,
-          log_status,
-          log_hash,
-          log_fid,
-          log_ago,
-          next_in,
-          hash_count
-        FROM (
-          SELECT
-            bin_to_uuid(svc.c_uuid, true) AS svc_uuid,
-            svc.c_data AS svc_data,
-            svc.c_type AS svc_type,
-            svc.c_name AS svc_name,
-            bin_to_uuid(act.c_uuid, true) AS act_uuid,
-            act.c_type AS act_type,
-            act.c_freq AS act_freq,
-            bin_to_uuid(log.c_uuid, true) as log_uuid,
-            IFNULL(log.c_ts_requested, 0) AS log_req,
-            IFNULL(log.c_ts_responded, 0) AS log_res,
-            TIMESTAMPDIFF(SECOND, log.c_ts_requested, c_ts_responded) AS log_duration,
-            log.c_status AS log_status,
-            log.c_response_hash AS log_hash,
-            log.c_response_fid AS log_fid,
-            TIMESTAMPDIFF(SECOND, log.c_ts_requested, NOW()) AS log_ago,
-            ifnull(act.c_freq - TIMESTAMPDIFF(SECOND, log.c_ts_requested, NOW()),-1) AS next_in,
-            (SELECT COUNT(*) FROM t_if__logs WHERE c_response_hash = log.c_response_hash) AS hash_count,
-            ROW_NUMBER() OVER (PARTITION BY act.c_uuid ORDER BY TIMESTAMPDIFF(SECOND, log.c_ts_requested, NOW())) AS row_num
-          FROM t_if__services svc
-          LEFT JOIN t_if__actions act ON svc.c_uuid = act.c_svc_uuid
-          LEFT JOIN t_if__logs log ON act.c_uuid = log.c_act_uuid
-          ORDER BY log_ago ASC, log_duration ASC
-        ) subquery";
-        $q['queue'] = "
-        SELECT
-          svc_uuid,
-          svc_type,
-          svc_name,
-          act_uuid,
-          act_type,
-          act_freq,
-          log_req,
-          log_res,
-          log_duration,
-          log_status,
-          log_hash,
-          log_fid,
-          log_ago,
-          next_in,
-          hash_count
-        FROM (
-          SELECT
-            bin_to_uuid(svc.c_uuid, true) AS svc_uuid,
-            svc.c_data AS svc_data,
-            svc.c_type AS svc_type,
-            svc.c_name AS svc_name,
-            bin_to_uuid(act.c_uuid, true) AS act_uuid,
-            act.c_type AS act_type,
-            act.c_freq AS act_freq,
-            bin_to_uuid(log.c_uuid, true) as log_uuid,
-            IFNULL(log.c_ts_requested, 0) AS log_req,
-            IFNULL(log.c_ts_responded, 0) AS log_res,
-            TIMESTAMPDIFF(SECOND, log.c_ts_requested, c_ts_responded) AS log_duration,
-            log.c_status AS log_status,
-            log.c_response_hash AS log_hash,
-            log.c_response_fid AS log_fid,
-            TIMESTAMPDIFF(SECOND, log.c_ts_requested, NOW()) AS log_ago,
-            ifnull(act.c_freq - TIMESTAMPDIFF(SECOND, log.c_ts_requested, NOW()),-1) AS next_in,
-            (SELECT COUNT(*) FROM t_if__logs WHERE c_response_hash = log.c_response_hash) AS hash_count,
-            ROW_NUMBER() OVER (PARTITION BY act.c_uuid ORDER BY TIMESTAMPDIFF(SECOND, log.c_ts_requested, NOW())) AS row_num
-          FROM t_if__services svc
-          LEFT JOIN t_if__actions act ON svc.c_uuid = act.c_svc_uuid
-          LEFT JOIN t_if__logs log ON act.c_uuid = log.c_act_uuid
-          ORDER BY log_ago ASC, log_duration ASC
-        ) subquery";
+        $q['rows:runs:joined'] = "
+            SELECT
+                bin_to_uuid(svc.c_uuid, true) AS deployment_uuid,
+                svc.c_data AS deployment_data,
+                svc.c_service AS service,
+                svc.c_name AS deployment_name,
+                svc.c_remote AS deployment_host,
+                bin_to_uuid(act.c_uuid, true) AS act_uuid,
+                act.c_scheme AS act_scheme,
+                act.c_freq AS act_freq,
+                bin_to_uuid(run.c_uuid, true) as run_uuid,
+                IFNULL(run.c_ts_start, 0) AS run_req,
+                IFNULL(run.c_ts_finish, 0) AS run_res,
+                run.c_status AS run_ok,
+                run.c_hash AS run_hash,
+                run.c_fid AS run_fid,
+                TIMESTAMPDIFF(SECOND, run.c_ts_finish, NOW()) AS run_ago,
+                IFNULL(act.c_freq - TIMESTAMPDIFF(SECOND, run.c_ts_finish, NOW()), -1) AS run_in,
+                TIMESTAMPDIFF(SECOND, c_ts_start, c_ts_finish) AS run_time,
+                (SELECT COUNT(*) FROM t_if__runs WHERE c_hash = run.c_hash) AS run_hash_count,
+                ROW_NUMBER() OVER (PARTITION BY COALESCE(act.c_uuid, UUID()) ORDER BY TIMESTAMPDIFF(SECOND, run.c_ts_finish, NOW())) AS row_num
+                -- Row_num is = 1 is used to select the latest run of an action.
+                -- Row_num is = null ensures we don't ge a null on actions that never had a run
+                -- COALESCE(act.c_uuid, UUID()) ensures listing in case service deployment doesn't have an action stored
+            FROM t_if__deployments svc
+            LEFT JOIN t_if__actions act ON svc.c_uuid = act.c_deployment_uuid
+            LEFT JOIN t_if__runs run ON act.c_uuid = run.c_action_uuid
+            ORDER BY run_ago ASC, run_duration ASC
+
+        ";
+
+        $q['json:runs:all'] = "
+        SELECT COALESCE(
+            JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'service', service,
+                    -- 'deployment', deployment_data,
+                    'deployment', JSON_MERGE_PRESERVE(deployment_data, JSON_OBJECT('uuid', deployment_uuid)),
+                    'action', JSON_OBJECT(
+                        'uuid', act_uuid,
+                        'scheme', act_scheme,
+                        'freq', act_freq
+                    ),
+                    'run', JSON_OBJECT(
+                        'uuid', run_uuid,
+                        'req', run_req,
+                        'res', run_res,
+                        'duration', run_duration,
+                        'ok', run_ok,
+                        'hash', run_hash,
+                        'fid', run_fid,
+                        'ago', run_ago,
+                        'in', run_in,
+                        'hash_count', run_hash_count
+                    )
+                )
+            ),
+            JSON_ARRAY()
+        ) AS json_result
+        FROM ( 
+            {$q['rows:runs:joined']}
+        ) subquery
+        ";
+
+        $q['json:runs:latest'] = "{$q['json:runs:all']} where (subquery.row_num = 1 or subquery.row_num is NULL)";
+
+
         $this->q = $q;
     }
 
