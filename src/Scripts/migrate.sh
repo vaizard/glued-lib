e7d07c2449e5e73f2d3db8386b39a6d4e00ad843
Pavel Stratil <pavel@vaizard.org>
2024-03-30 17:33:30 +0000

    mirgrate shit

--- src/Scripts/migrate.sh
+++ src/Scripts/migrate.sh
@@ -7,37 +7,19 @@ source "$DIR/loadenv.sh"
 # NOTE that double sourcing is needed
 # otherwise .env references won't be interpreted
 
-if ! mysql -u $MYSQL_USERNAME -p"${MYSQL_PASSWORD}" -h ${MYSQL_HOSTNAME} -e "use $MYSQL_DATABASE"; then
-  echo "[WARN] Connecting to database $MYSQL_DATABASE failed."
-  echo "[INFO] Attempting to create database and assign privileges.";
-  mysql -e "CREATE DATABASE $MYSQL_DATABASE /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-  mysql -e "CREATE USER $MYSQL_USERNAME@$MYSQL_HOSTNAME IDENTIFIED BY '$MYSQL_PASSWORD';"
-  mysql -e "GRANT ALL PRIVILEGES ON glued.* TO '$MYSQL_USERNAME'@'$MYSQL_HOSTNAME';"
-  mysql -e "FLUSH PRIVILEGES;"
-  exit;
-fi
+# Loop over both Mysql and Pgsql directories
+for dbtype in Mysql Pgsql; do
+  # Find non-empty directories within each specified path
+  for dir in $(find "./glued/Config/${dbtype}" -not -empty -type d); do
 
-# Connect to PostgreSQL database
-if ! PGPASSWORD="${PGSQL_PASSWORD}" psql -U "${PGSQL_USERNAME}" -h "${PGSQL_HOSTNAME}" -d "${PGSQL_DATABASE}" -c "\q"; then
-  echo "[WARN] Connecting to database $PGSQL_DATABASE failed."
-  echo "[INFO] Attempting to create database and assign privileges."
+    # Set DATABASE_URL environment variable based on dbtype
+    if [ "${dbtype}" == "Mysql" ]; then export DATABASE_URL="${MYSQL_URL}"; fi
+    if [ "${dbtype}" == "Pgsql" ]; then export DATABASE_URL="${PGSQL_URL}"; fi
 
-  # Create Database
-  PGPASSWORD="${PGSQL_PASSWORD}" psql -U "${PGSQL_USERNAME}" -h "${PGSQL_HOSTNAME}" -d "postgres" -c "CREATE DATABASE ${PGSQL_DATABASE} WITH ENCODING='UTF8';"
+    # Execute dbmate with the appropriate directory and schema file (convert dbtype to lowercase)
+    dbmate -d "${dir}" -s "${DATAPATH}/$(basename `pwd`)/schema-${dbtype,,}.sql" migrate
+  done
+done
 
-  # Create User and Grant Privileges
-  PGPASSWORD="${PGSQL_PASSWORD}" psql -U "${PGSQL_USERNAME}" -h "${PGSQL_HOSTNAME}" -d "${PGSQL_DATABASE}" <<EOSQL
-CREATE USER ${PGSQL_USERNAME} WITH PASSWORD '${PGSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON DATABASE ${PGSQL_DATABASE} TO ${PGSQL_USERNAME};
-EOSQL
-fi
+echo "[DONE] $(basename $0) -----------------"
 
-for dir in $(find ./glued/Config/Migrations/mysql -not -empty -type d) ; do 
-  dbmate -d "${dir}" -s "${DATAPATH}/$(basename `pwd`)/mysql-schema.sql" migrate;
-done;
-
-for dir in $(find ./glued/Config/Migrations/pgsql -not -empty -type d) ; do 
-  dbmate -d "${dir}" -s "${DATAPATH}/$(basename `pwd`)/pgsql-schema.sql" migrate;
-done;
-
-echo "[PASS] migrated"
