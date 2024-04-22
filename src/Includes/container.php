6e7702aac9c2929b1ffa9c2b635054cfc3460fb3
Pavel Stratil <pavel@vaizard.org>
2024-04-22 09:37:30 +0200

    Add container include deps

--- src/Includes/container.php
+++ src/Includes/container.php
@@ -5,6 +5,8 @@ use DI\Container;
 use Psr\Log\NullLogger;
 use Grasmash\YamlExpander\YamlExpander;
 use Symfony\Component\Yaml\Yaml;
+use \PDO;
+use \PDOException;
 
 $container->set('settings', function () {
     // Initialize
@@ -44,18 +46,18 @@ $container->set('pg', function (Container $c) {
     $cnf = $c->get('settings')['pgsql'];
     $dsn = "pgsql:host={$cnf['host']};dbname={$cnf['database']};options='--client_encoding={$cnf['charset']}'";
     $options = [
-        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-        \PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
     ];
     try {
-        $conn = new \PDO(
+        $conn = new PDO(
             $dsn,
             $cnf['username'],
             $cnf['password'],
             $options);
         $conn->exec("SET search_path TO {$cnf['schema']}");
-    } catch (\PDOException $e) {
+    } catch (PDOException $e) {
         throw new \Exception($e->getMessage());
     }
     return $conn;
