1f1ee7726dc3dcc9087f1b7c1956b9966141e2b7
Pavel Stratil <pavel@vaizard.org>
2024-04-23 03:05:19 +0200

    Suppert querying by uuid on deployments

--- src/IfUtils.php
+++ src/IfUtils.php
@@ -13,12 +13,14 @@ class IfUtils
         $this->settings = $settings;
     }
 
-    public function getDeploymentsJson($service = false): string {
+    public function getDeploymentsJson($service = false, $uuid = false): string {
         $base = $this->settings['glued']['baseuri'] . $this->settings['routes']['be_if']['path'] . '/svc/';
-        $q = "SELECT json_agg(json_build_object('uuid', uuid, 'conn', conn, 'meta', meta, 'uri', concat('{$base}',service,'/latest/',uuid))) AS deployments FROM if__deployments";
-        if ($service) { $q .= " WHERE service = :service"; }
+        $q = "SELECT json_agg(json_build_object('uuid', uuid, 'conn', conn, 'meta', meta, 'uri', concat('{$base}',service,'/latest/',uuid))) AS deployments FROM if__deployments WHERE 1=1 ";
+        if ($service) { $q .= " AND service = :service "; }
+        if ($uuid) { $q .= " AND uuid = :uuid "; }
         $stmt = $this->pg->prepare($q);
         if ($service) { $stmt->bindValue(':service', $service, \PDO::PARAM_STR); }
+        if ($service) { $stmt->bindValue(':uuid', $uuid, \PDO::PARAM_STR); }
         $stmt->execute();
         $res = $stmt->fetch(\PDO::FETCH_ASSOC);
         return $res['deployments'] ?? '{}';
