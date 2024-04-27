43e946912f6c196f485087eb1da5df755edc7952
Pavel Stratil <pavel@vaizard.org>
2024-04-27 18:22:19 +0200

    expand common container base

--- src/Includes/container.php
+++ src/Includes/container.php
@@ -6,8 +6,40 @@ use Psr\Log\NullLogger;
 use Grasmash\YamlExpander\YamlExpander;
 use Symfony\Component\Yaml\Yaml;
 use Glued\Lib\IfUtils;
+use Glued\Lib\Crypto;
+use Glued\Lib\Utils;
+use Opis\JsonSchema\Validator;
 use \PDO;
 use \PDOException;
+use Phpfastcache\CacheManager;
+use Phpfastcache\Config\ConfigurationOption;
+use Phpfastcache\Helper\Psr16Adapter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\UidProcessor;
+use Monolog\Handler\TelegramBotHandler;
+use Monolog\Handler\FallbackGroupHandler;
+use Monolog\Handler\DeduplicationHandler;
+
+$container->set('fscache', function () {
+    try {
+        $path = $_ENV['DATAPATH'] . '/' . basename(__ROOT__) . '/cache/psr16';
+        CacheManager::setDefaultConfig(new ConfigurationOption([
+            "path" => $path,
+            "itemDetailedDate" => false,
+        ]));
+        return new Psr16Adapter('files');
+    } catch (Exception $e) {
+        throw new \Exception("Path {$path} not writable. Rerun `composer configure`. {$e->getMessage()}", $e->getCode());
+    }
+});
+
+$container->set('memcache', function () {
+    CacheManager::setDefaultConfig(new ConfigurationOption([
+        "defaultTtl" => 60,
+    ]));
+    return new Psr16Adapter('apcu');
+});
 
 $container->set('settings', function () {
     // Initialize
@@ -67,3 +99,68 @@ $container->set('pg', function (Container $c) {
 $container->set('ifutils', function (Container $c) {
     return new IfUtils($c->get('pg'), $c->get('settings'));
 });
+
+$container->set('my', function (Container $c) {
+    $db = $c->get('settings')['mysql'];
+    $mysqli = new mysqli($db['host'], $db['username'], $db['password'], $db['database']);
+    $mysqli->set_charset($db['charset']);
+    $mysqli->query("SET collation_connection = " . $db['collation']);
+    return $mysqli;
+});
+
+$container->set('logger', function (Container $c)
+{
+    // Create the main logger, add the UidProcessor to generate a unique id to each log record
+    $settings = $c->get('settings')['logger'];
+    $logger = new Logger($settings['name']);
+    $processor = new UidProcessor();
+    $logger->pushProcessor($processor);
+
+    // Create handler: stream to file
+    $handlers['stream'] = new StreamHandler($settings['path'], $settings['level']);
+    // Create handler: telegram
+    $tg = $c->get('settings')['notify']['network']['telegram'] ?? null;
+
+    if (!is_null($tg['channels'][0]['dsn'])) {
+        // Extract the query string from the DSN
+        $chatid = $tg['dst'][0];
+        $apikey = parse_url($tg['channels'][0]['dsn'], PHP_URL_USER) . ":" . parse_url($tg['channels'][0]['dsn'], PHP_URL_PASS);
+        // Create the TelegramBotHandler
+        $handlers['tg'] = new TelegramBotHandler(
+            apiKey: $apikey,
+            channel: $chatid,
+            level: $settings['level'],
+            parseMode: 'Markdown',
+            delayBetweenMessages: true
+        );
+    }
+
+    // Avoid app failure in case of logger failure (i.e. don't throw an exception if the stream logger path
+    // is not writable. Use telegram, e-mail, etc. Deduplicate same error messages to prevent worst spamming.
+    $handler = new FallbackGroupHandler($handlers);
+    $handler = new DeduplicationHandler($handler);
+    $logger->pushHandler($handler);
+
+    if (!is_writable($settings['path'])) { $logger->error('Log path not writable.', [$settings['path']]); }
+    return $logger;
+});
+
+$container->set('transform', function () {
+    return new ArrayTransformer();
+});
+
+$container->set('jsonvalidator', function () {
+    return new Validator;
+});
+
+$container->set('routecollector', $app->getRouteCollector());
+$container->set('responsefactory', $app->getResponseFactory());
+
+$container->set('utils', function (Container $c) {
+    return new Utils($c->get('db'), $c->get('settings'), $c->get('routecollector'));
+});
+
+$container->set('crypto', function () {
+    return new Crypto();
+});
+
