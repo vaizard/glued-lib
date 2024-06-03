4c94b7d2d0728e62eface93bb97006bb39e0119f
Pavel Stratil <pavel@vaizard.org>
2024-06-04 00:07:48 +0200

    add $event global to environment.php

--- src/Includes/environment.php
+++ src/Includes/environment.php
@@ -6,5 +6,8 @@ $settings = $container->get('settings');
 foreach ($settings['php']['ini'] as $key => $value) {
     ini_set($key, $value);
 }
+
 error_reporting($settings['php']['error_reporting'] ?? 1);
 date_default_timezone_set($settings['glued']['timezone']);
+
+$event = $container->get('events');
