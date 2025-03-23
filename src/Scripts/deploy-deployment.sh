ed3213697e4ad45660ba756a68e2db7eac39296b
Pavel Stratil <pavel@vaizard.org>
2025-03-24 00:23:03 +0100

    added deploy-deployment.sh

---
+++ src/Scripts/deploy-deployment.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "$DIR/loadenv.sh"
+source "$DIR/loadenv.sh"
+
+echo "[INFO] deploying deployment"
+
+cp -r glued/Config/Deployment/*.yaml "${DATAPATH}/$(basename `pwd`)/config"
+mkdir "${DATAPATH}/$(basename `pwd`)/crons"
+cp -r glued/Config/Cron/* "${DATAPATH}/$(basename `pwd`)/crons"
