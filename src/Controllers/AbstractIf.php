2c89b590413061d51bd2dcce480df3b27cc25562
Pavel Stratil <pavel@vaizard.org>
2025-04-13 01:24:10 +0200

    dropped the unused if/actions abstraction

--- src/Controllers/AbstractIf.php
+++ src/Controllers/AbstractIf.php
@@ -15,17 +15,11 @@ abstract class AbstractIf extends AbstractService
      */
     protected $deployments;
 
-
     /**
      * @var array
      */
     protected array $deployment;
 
-
-    /**
-     * @var \Glued\Lib\Sql
-     */
-    protected $actions;
 
     /**
      * @var array of ssh tunnelling ports to close
@@ -35,7 +29,7 @@ abstract class AbstractIf extends AbstractService
 
     /**
      * Constructor, initializes the class with a given container, setting up the deployments
-     * and actions SQL handlers and initializing the deployment array.
+     * SQL handler and initializing the deployment and closePorts array.
      *
      * @param ContainerInterface $container The container instance for dependency injection.
      */
@@ -43,7 +37,6 @@ abstract class AbstractIf extends AbstractService
     {
         parent::__construct($container);
         $this->deployments = new \Glued\Lib\Sql($this->pg, 'if__deployments');
-        $this->actions = new \Glued\Lib\Sql($this->pg, 'if__actions');
         $this->deployment = [];
         $this->closePorts = [];
 
@@ -69,8 +62,8 @@ abstract class AbstractIf extends AbstractService
         return $response->withJson($data);
     }
 
-/**
-     * Retrieves the deployment details including actions for a given deployment UUID.
+    /**
+     * Retrieves the deployment details for a given deployment UUID.
      *
      * @param string $deploymentUUID The UUID of the deployment to retrieve.
      * @return mixed The deployment details as an associative array if found, false otherwise.
@@ -78,15 +71,15 @@ abstract class AbstractIf extends AbstractService
     protected function getDeployment($deploymentUUID): mixed
     {
         $query = <<<EOT
-        SELECT 
-            jsonb_build_object('nonce', id.nonce, 'created_at', id.created_at, 'updated_at', id.updated_at) || 
-            id.{$this->deployments->dataColumn} || 
-            jsonb_build_object('actions', COALESCE(jsonb_agg(ia.doc - 'deployment') FILTER (WHERE ia.doc IS NOT NULL), '[]'::jsonb))
-            AS doc
-        FROM glued.if__deployments id 
-        LEFT JOIN glued.if__actions ia ON id.uuid = ia.deployment 
-        WHERE id.uuid = :uuid
-        GROUP BY id.doc, id.nonce, id.created_at, id.updated_at;
+            SELECT 
+                jsonb_build_object(
+                    'nonce', id.nonce,
+                    'created_at', id.created_at,
+                    'updated_at', id.updated_at
+                ) || 
+                id.{$this->deployments->dataColumn} AS doc
+            FROM glued.if__deployments id 
+            WHERE id.uuid = :uuid;
         EOT;
         $stmt = $this->deployments->pdo->prepare($query);
         $stmt->bindParam(':uuid', $deploymentUUID);
@@ -118,78 +111,8 @@ abstract class AbstractIf extends AbstractService
         throw new \Exception("Bad interface configuration / interface connector missing in {$deploymentUUID}",500);
     }
 
-
     /**
-     * Retrieves or creates the `action` UUID of a given { deployment UUID, request path, and request method }
-     * combination.
-     *
-     * @param string $deploymentUUID The UUID of the deployment.
-     * @param string $requestPath The request path.
-     * @param string $requestMethod The request method.
-     * @return mixed The action UUID if found or created, or null otherwise.
-     */
-    public function getActionUUID($deploymentUUID, $requestPath, $requestMethod): mixed
-    {
-        if (empty($this->deployment)) { $this->getDeployment($deploymentUUID); }
-        $uuid = array_column(array_filter($array['actions'] ?? [], fn($action) => $action['path'] === $path && $action['method'] === $method), 'uuid')[0] ?? null;
-        if (empty($uuid)) {
-            $action = [
-                'deployment' => $deploymentUUID,
-                'method' => $requestMethod,
-                'path' => $requestPath
-            ];
-            $uuid = $this->actions->create($action, true, true);
-        }
-        return $uuid;
-    }
-
-    /**
-     * Cache valid action responses
-     *
-     * This method inserts a new record into the `if__actions_valid_response_cache` table. If a record with the same
-     * nonce already exists, it updates the `req_at` timestamp and increments the `res_replays` counter.
-     *
-     * The `nonce` is a MD5 hash of the concatenation of `action_uuid`, `req_payload`, `req_params`, `res_payload`
-     * and `res_id`.
-     *
-     * @param string $actionUUID The UUID of the action.
-     * @param string $reqParams The request parameters as a string (leave empty if none provided).
-     * @param string $reqPayload The request payload as a JSON formatted string (leave empty if none provided).
-     * @param string $resPayload The response payload in JSON format (leave empty if none provided).
-     * @param string $fid The foreign ID of the request (leave empty if none provided).
-     *
-     * @return mixed Returns the UUID of the `action_uuid`, `req_payload`, `req_params`, `res_payload` combination.
-     *
-     * @throws \PDOException If there is a database error.
-     */
-    public function cacheValidActionsResponse(string $actionUUID, array $reqParams = [], array $reqPayload = [], array $resPayload = [], string $fid = ""): string
-    {
-        $reqParams = json_encode($reqParams);
-        $reqPayload = json_encode($reqPayload);
-        $resPayload = json_encode($resPayload);
-        $sql = <<<EOL
-        INSERT INTO {$this->deployments->schema}.if__actions_valid_response_cache 
-        (action_uuid, req_payload, req_params, res_payload, fid) 
-        VALUES 
-        (:actionUUID, :reqPayload::jsonb, :reqParams::jsonb, :resPayload::jsonb, :fid) 
-        ON CONFLICT (nonce) DO UPDATE SET                           
-            req_at = CURRENT_TIMESTAMP,
-            res_replays = if__actions_valid_response_cache.res_replays + 1 
-        RETURNING encode(nonce,'hex')
-        EOL;
-        $stmt = $this->deployments->pdo->prepare($sql);
-        $stmt->bindParam(':actionUUID', $actionUUID);
-        $stmt->bindParam(':reqPayload', $reqPayload);
-        $stmt->bindParam(':reqParams', $reqParams);
-        $stmt->bindParam(':resPayload', $resPayload);
-        $stmt->bindParam(':fid', $fid);
-        $stmt->execute();
-        return $stmt->fetchColumn();
-    }
-
-    /**
-     * Returns a list of endpoints within a deployments and creates/updates actions (the deployment, request path
-     * and request method triplets stored in the if__actions table).
+     * Returns a list of endpoints within a deployments.
      *
      * @param Request $request The HTTP request object.
      * @param Response $response The HTTP response object.
@@ -198,25 +121,15 @@ abstract class AbstractIf extends AbstractService
      */
     public function methods(Request $request, Response $response, array $args = []): Response
     {
-        $actionUUID = $this->getActionUUID($args['deployment'], (string) $request->getUri()->getPath(), (string) $request->getMethod());
         $service = explode("/", (string) $request->getUri()->getPath())[4];
         $filteredRoutes = array_filter($this->settings["routes"], function ($route) use ($service) {
             return isset($route["service"]) && strpos($route["service"], "if/{$service}") === 0;
         });
         foreach ($filteredRoutes as $r) {
-            foreach ($r['methods'] as $method=>$call) {
-                $action = [
-                    'deployment' => $args['deployment'],
-                    'method' => $method,
-                    'path' => str_replace('{deployment}', $args['deployment'], $r['pattern'])
-                ];
-                $this->actions->create($action, true, true);
-            }
             $res[] = [
                 'label' => $r['label'],
                 'dscr' => $r['dscr'],
                 'uri' => $this->settings['glued']['baseuri'].str_replace('{deployment}', $args['deployment'], $r['pattern']),
-                'uuid' => $actionUUID
             ];
         }
         return $response->withJson($res);
