2ec1fc450d12bbf9db2b13e3a1d9bbcd556b5522
Pavel Stratil <pavel@vaizard.org>
2025-04-20 00:31:48 +0200

    Allowed ExtendedException to carry array or string details.

--- src/Exceptions/ExtendedException.php
+++ src/Exceptions/ExtendedException.php
@@ -5,7 +5,7 @@ namespace Glued\Lib\Exceptions;
 class ExtendedException extends \Exception
 {
     protected $details;
-    public function __construct($message = "", $code = 0, array $details = [])
+    public function __construct($message = "", $code = 0, string|array $details = [])
     {
         parent::__construct($message, $code);
         $this->details = $details;
