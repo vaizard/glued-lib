df254b11403988f4b9c30862f12ecf9e1bcb7746
Pavel Stratil <pavel@senbonzakura.net>
2025-08-27 15:05:28 +0200

    revert

--- src/TsSql.php
+++ src/TsSql.php
@@ -160,8 +160,183 @@ class TsSql extends Sql {
         return $this->pdo->commit();
     }
 
+    /*
+    public function CommonCreateBatch(array | object $docs, $xf)
+    {
+        throw new \Exception("Method deprecated, use insertBatchUpstreamAndInternal() instead.");
+    }*/
+
+    /**
+     * Stores upstream data ($docs) uniquely identified by the external unique key
+     * $this->extKey. A database insert is perform always when the latest $doc in database
+     * differs from the $doc provided by upstream (also when reverting a $doc to any of its
+     * previous states). Furthermore, $docs are transformed to their internal representation
+     * and stored as well.
+     * a $doc
+     * @param array|object $docs array of documents passed from upstream interface
+     * @param $xf Selective\Transformer object ensuring transformation of upstream docs
+     * to their internal representation     * @return bool
+     */
+    public function CommonCreateBatch(array | object $docs, $xf)
+    {
+        $qInternal = "
+            INSERT INTO glued.{$this->tableInternal} (doc, created_at)
+            VALUES (:doc, :created_at)
+            ON CONFLICT (nonce, created_at) DO NOTHING;
+        ";
+
+        $qUpstream = "
+            INSERT INTO {$this->tableUpstream} (uuid, doc)
+            SELECT
+                COALESCE(latest.uuid, gen_random_uuid()),
+                upstream.doc
+            FROM (
+                 SELECT
+                     :doc::jsonb AS doc,
+                     (:doc::jsonb ->> '{$this->extKey}') AS ext_id,
+                     DECODE(MD5(:doc::jsonb::TEXT), 'hex') AS nonce
+                 ) AS upstream
+            LEFT JOIN LATERAL (
+                SELECT uuid, nonce
+                FROM {$this->tableUpstream}
+                WHERE ext_id = upstream.ext_id
+                ORDER BY created_at DESC
+                LIMIT 1
+            ) AS latest ON TRUE
+            WHERE latest.nonce IS DISTINCT FROM upstream.nonce
+               OR latest.nonce IS NULL
+            RETURNING uuid, created_at;
+        ";
+
+        $this->stmtInternal = $this->pdo->prepare($qInternal);
+        $this->stmtUpstream = $this->pdo->prepare($qUpstream);
+
+        $this->pdo->beginTransaction();
+        foreach ($docs as $doc) {
+            $jsonDoc = json_encode($doc);
+            $this->stmtUpstream->bindParam(':doc', $jsonDoc);
+            $this->stmtUpstream->execute();
+            $insert = $this->stmtUpstream->fetch(\PDO::FETCH_ASSOC);
+            if ($insert) {
+                $doc['uuid'] = $insert['uuid'];
+                $jsonDoc = json_encode($xf->toArray($doc));
+                $this->stmtInternal->bindParam(':doc', $jsonDoc);
+                $this->stmtInternal->bindParam(':created_at', $insert['created_at']);
+                $this->stmtInternal->execute();
+            }
+        }
+        return $this->pdo->commit();
+    }
+}
+
+
+class TsSql2 extends Sql2 {
+    /** @var \PDOStatement The PDOStatement instance for upstream database queries. */
+    public $stmtUpstream;
+
+    /** @var \PDOStatement The PDOStatement instance for internal (transformed upstream) database queries. */
+    public $stmtInternal;
+
+    /** @var string The table name for database queries handling raw upstream data. */
+    public string $tableUpstream;
+
+    /** @var string The table name for database queries handling transformed data. */
+    public string $tableInternal;
+
+    /** @var array An array of metadata columns that are to be auto-appended to the `doc` object. Overriding the GenericSQL class here. */
+    public array $metaColumns = ['nonce', 'created_at'];
+
+    /** @var string Name of the external data unique key. */
+    public string $extKey;
+
+    public function __construct(PDO $pdo, string $table, $extKey)
+    {
+        // Call the parent constructor
+        parent::__construct($pdo, $table);
+
+        // Extend the constructor by adding more logic
+        $this->tableUpstream = "{$table}_ext";
+        $this->tableInternal = "{$table}";
+        $this->extKey = $extKey;
+    }
+
+
+
+    /**
+     * Inserts a batch of upstream json documents and their internal representations.
+     *
+     * This method processes a collection of JSON documents received from an upstream interface,
+     * ensuring that each document is stored uniquely based on an external unique key specified by
+     * `$this->extKey`. The upstream table is updated only if the most recent document stored (identified
+     * by the external key and a computed nonce from the document's JSON) differs from the incoming document,
+     * or if no record exists for that external identifier.
+     *
+     * For each document that results in a new insertion (or an update to a previous state) in the upstream table,
+     * the method retrieves the generated UUID and creation timestamp. It then uses the provided transformer
+     * (`$transformer`) to convert the upstream document into its internal representation and inserts the transformed document
+     * into the internal table using the returned creation timestamp.
+     *
+     * Both the upstream and internal insertions are executed within a single transaction for atomicity.
+     *
+     * @param array|object $docs An array or iterable collection of JSON documents received from the upstream interface.
+     * @param \Selective\Transformer $transformer A transformer object that converts upstream documents to their internal representation.
+     * @return bool Returns true if the transaction commits successfully; false otherwise.
+     */
+    public function insertBatchUpstreamAndInternal(array | object $docs, $transformer)
+    {
+        $qInternal = "
+            INSERT INTO glued.{$this->tableInternal} (doc, created_at)
+            VALUES (:doc, :created_at)
+            ON CONFLICT (nonce, created_at) DO NOTHING;
+        ";
+
+        $qUpstream = "
+            INSERT INTO {$this->tableUpstream} (uuid, doc)
+            SELECT
+                COALESCE(latest.uuid, gen_random_uuid()),
+                upstream.doc
+            FROM (
+                 SELECT
+                     :doc::jsonb AS doc,
+                     (:doc::jsonb ->> '{$this->extKey}') AS ext_id,
+                     DECODE(MD5(:doc::jsonb::TEXT), 'hex') AS nonce
+                 ) AS upstream
+            LEFT JOIN LATERAL (
+                SELECT uuid, nonce
+                FROM {$this->tableUpstream}
+                WHERE ext_id = upstream.ext_id
+                ORDER BY created_at DESC
+                LIMIT 1
+            ) AS latest ON TRUE
+            WHERE latest.nonce IS DISTINCT FROM upstream.nonce
+               OR latest.nonce IS NULL
+            RETURNING uuid, created_at;
+        ";
+
+        $this->stmtInternal = $this->pdo->prepare($qInternal);
+        $this->stmtUpstream = $this->pdo->prepare($qUpstream);
+
+        $this->pdo->beginTransaction();
+        foreach ($docs as $doc) {
+            $jsonDoc = json_encode($doc);
+            $this->stmtUpstream->bindParam(':doc', $jsonDoc);
+            $this->stmtUpstream->execute();
+            $insert = $this->stmtUpstream->fetch(\PDO::FETCH_ASSOC);
+            if ($insert) {
+                $doc['uuid'] = $insert['uuid'];
+                $jsonDoc = json_encode($transformer->toArray($doc));
+                $this->stmtInternal->bindParam(':doc', $jsonDoc);
+                $this->stmtInternal->bindParam(':created_at', $insert['created_at']);
+                $this->stmtInternal->execute();
+            }
+        }
+        return $this->pdo->commit();
+    }
+
+    /*
     public function CommonCreateBatch(array | object $docs, $xf)
     {
         throw new \Exception("Method deprecated, use insertBatchUpstreamAndInternal() instead.");
     }
+*/
 }
